e5743c8d47d8166be3071249284fad72d30f1939
Priyanka-500apps <priyanka.varakolu@500apps.com>
2023-03-13 20:07:21 +0530

    File Upload component

---
+++ file-upload/src/components/FileUpload/FileUpload.ts
@@ -0,0 +1,37 @@
+import { defineComponent, ref } from "vue";
+
+export default defineComponent({
+
+    name: "FileUpload",
+
+    setup() {
+        const file = ref<File | null>();
+        const form = ref<HTMLFormElement>();
+
+        function onFileChanged($event: Event) {
+            console.log("dfdfhdf", $event);
+            const target = $event.target as HTMLInputElement;
+            if (target && target.files) {
+                file.value = target.files[0];
+            }
+        }
+
+        async function saveImage() {
+            if (file.value) {
+                try {
+                // save file.value
+                } catch (error) {
+                    console.error(error);
+                    form.value?.reset();
+                    file.value = null;
+                } finally {
+                }
+            }
+        };
+
+        return {
+            saveImage,
+            onFileChanged,
+        }
+    }
+});
---
+++ file-upload/tailwind.config.cjs
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js,vue}",
+    "node_modules/flowbite/**/*.{js,jsx,ts,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [require("flowbite/plugin"), require("autoprefixer"), require("flowbite/plugin")],
+};
